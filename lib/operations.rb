82795be6b5c8c86badcd2edcd1bb56cba62eeec4
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-21 03:35:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,9 +2,9 @@ speed = 80
 
 def unsafe?(speed)
   return true if speed > 60
-    puts "Is an unsafe speed!"
+    return "Is an unsafe speed!"
   return true elsif speed < 40
-    puts "Is an unsafe speed!"
+    return "Is an unsafe speed!"
   end
 end
 
